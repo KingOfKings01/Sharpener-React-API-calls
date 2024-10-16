85973fd7c86250e9c1011d5a1a86937a12532e62
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-16 19:36:56 +0530

    Set error messages if Api faild. Create re-fetching function with the interval of 5 second. Next set a cancle retring button to stop re-fetching.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,88 @@
-import { useState } from 'react';
-import './App.css'
-import MovieList from './components/Movie/MovieList'
+import { useState, useRef } from 'react';
+import './App.css';
+import MovieList from './components/Movie/MovieList';
 
 function App() {
-  
-  const dummyData = [{
-    id: 1,
-    title: 'The Godfather',
-    release_date: "1972-01-20",
-    opening_crawl: 'Francis Ford Coppola',
-  },
-  {
-    id: 2,
-    title: 'Pulp Fiction',
-    release_date: "1994-03-29",
-    opening_crawl: 'Quentin Tarantino',
-  }
-]
+  const dummyData = [
+    {
+      id: 1,
+      title: 'The Godfather',
+      release_date: "1972-01-20",
+      opening_crawl: 'Francis Ford Coppola',
+    },
+    {
+      id: 2,
+      title: 'Pulp Fiction',
+      release_date: "1994-03-29",
+      opening_crawl: 'Quentin Tarantino',
+    }
+  ];
 
-const [movieList, setMovieList] = useState([]);
-const [isLoading, setIsLoading] = useState(false);
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [retry, setRetry] = useState(false);
+  const retryTimeoutRef = useRef(null);
 
   const fetchMovies = async () => {
-    try{
-      setIsLoading(true)
-      const response = await fetch('https://swapi.dev/api/films')
-      
-      const data = await response.json()
-      
-      setMovieList(data.results)
-      setIsLoading(false)
-    } catch(err){
-      console.error('Error fetching movies:', err)
-      setMovieList([])
-      setIsLoading(false)
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+      const data = await response.json();
+      setMovieList(data.results);
+      setError("");
+    } catch (err) {
+      console.error('Error fetching movies:', err);
+      setError(err.message);
+      setMovieList([]);
+      if (retry) {
+        retryTimeoutRef.current = setTimeout(fetchMovies, 5000);
+      }
     }
+    setIsLoading(false);
+  };
+
+  const retryFetch = () => {
+    setRetry(true);
+    fetchMovies();
+  };
+
+  const cancelRetry = () => {
+    setRetry(false);
+    clearTimeout(retryTimeoutRef.current);
+    setError("Cancelled retry");
+  };
+
+
+  let content = <p>No movies found!</p>;
+
+  if (movieList.length > 0) {
+    content = <MovieList movieList={movieList} isLoading={isLoading} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
   }
 
   return (
     <>
       <section>
-        <button onClick={fetchMovies}>Fetch Movies</button>
+        <button onClick={retryFetch}>Fetch Movies</button>
+        {retry && error && movieList.length == 0 && <button onClick={cancelRetry}>Cancel Retry</button>}
       </section>
       <section>
-      <MovieList  movieList={movieList} isLoading={isLoading}/>
+        <h1>MovieList</h1>
+        {content}
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Movie/MovieList.jsx
+++ src/components/Movie/MovieList.jsx
@@ -2,18 +2,13 @@
 export default function MovieList({ movieList, isLoading }) {
     return (
         <>
-            <h1>MovieList</h1>
-            {isLoading ? <p>Loading...</p> :
-                <>
-                    {movieList.map((movie, index) => (
-                        <div key={index}>
-                            <h2>{movie.title}</h2>
-                            <p>{movie.release_date}</p>
-                            <p>{movie.opening_crawl}</p>
-                        </div>
-                    ))}
-                </>
-            }
+            {movieList.map((movie, index) => (
+                <div key={index}>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.release_date}</p>
+                    <p>{movie.opening_crawl}</p>
+                </div>
+            ))}
         </>
     )
 }
